09e90c05507340e70f3c5f7e38249b84c2b38854
Jung Lee <lee.jung.s95@gmail.com>
2021-05-31 19:11:46 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -7,7 +7,12 @@ class Helpers
 
     end
 
-    def self.is_logged_in?
+    def self.is_logged_in?(session)
+      #You also have !!, which is not a method:
+      #This one is useful because it’ll turn any value into a boolean.
+      #Then you have !, which is the same but it gives you the opposite boolean value.
+      if
+
 
     end
 end
